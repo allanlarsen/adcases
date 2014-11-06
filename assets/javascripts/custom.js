4ebfb0c191ecda73cebf1375e96df9ab63d5026f
allan.larsen <allan.larsen@advicedigital.dk>
2014-11-06 12:27:00 +0100

    Add burgermenu and off-canvas

---
+++ assets/javascripts/custom.js
@@ -0,0 +1,106 @@
+/**
+ * Created by aduser on 06/11/14.
+ */
+
+
+/*====================================
+ =            HIDE HEADER           =
+ ====================================*/
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+
+    lastScrollTop = st;
+}
+
+/*====================================
+ =            ON DOM READY            =
+ ====================================*/
+$(function() {
+    $('.toggle-nav').click(function() {
+        // Calling a function in case you want to expand upon this.
+        toggleNav();
+    });
+});
+
+/*========================================
+ =            CUSTOM FUNCTIONS            =
+ ========================================*/
+function toggleNav() {
+    if ($('#site-wrapper').hasClass('show-nav')) {
+        // Do things on Nav Close
+        $('#site-wrapper').removeClass('show-nav');
+    } else {
+        // Do things on Nav Open
+        $('#site-wrapper').addClass('show-nav');
+    }
+
+    //$('#site-wrapper').toggleClass('show-nav');
+}
+
+/*========================================
+ =            CUSTOM FUNCTIONS            =
+ ========================================*/
+var Menu = {
+
+    el: {
+        ham: $('.menu'),
+        menuTop: $('.menu-top'),
+        menuMiddle: $('.menu-middle'),
+        menuBottom: $('.menu-bottom')
+    },
+
+    init: function() {
+        Menu.bindUIactions();
+    },
+
+    bindUIactions: function() {
+        Menu.el.ham
+            .on(
+            'click',
+            function(event) {
+                Menu.activateMenu(event);
+                event.preventDefault();
+            }
+        );
+    },
+
+    activateMenu: function() {
+        Menu.el.menuTop.toggleClass('menu-top-click');
+        Menu.el.menuMiddle.toggleClass('menu-middle-click');
+        Menu.el.menuBottom.toggleClass('menu-bottom-click');
+    }
+};
+
+Menu.init();
